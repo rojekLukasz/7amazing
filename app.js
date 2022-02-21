d770c07ee7ee72ebd5fc9833168f27a0c181895f
rojekLukasz <lukasz_grzegorz@purplebeard.co.uk>
2022-02-21 13:36:13 +0000

    images and sources

---
+++ app.js
@@ -0,0 +1,59 @@
+//Silbury Hill, Avebury
+//        https://www.english-heritage.org.uk/siteassets/home/visit/places-to-visit/silbury-hill/silbury-hill-hero.jpg?w=1440&h=612&mode=crop&scale=both&quality=60&anchor=&WebsiteVersion=20211126150055
+//        https://cdn.pixabay.com/photo/2018/11/15/16/33/silbury-hill-3817564_960_720.jpg
+//        https://www.english-heritage.org.uk/siteassets/home/visit/places-to-visit/silbury-hill/silbury-snow.jpg?w=1440&h=612&mode=crop&scale=both&quality=60&anchor=&WebsiteVersion=20211126150055
+
+//Uffington White Horse
+
+//         https://nt.global.ssl.fastly.net/images/1431736113455-whh-yq3w2924-nt-commission-air.jpg?width=1920&auto=webp&crop=16:7
+//         https://nt.global.ssl.fastly.net/images/1431768532670-1101374-nti-chris-lacey-resized.jpg?width=1920&auto=webp&crop=16:7
+//         https://images.unsplash.com/photo-1498832229091-4425cea65b29?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80
+
+// Stonehenge
+//          https://images.unsplash.com/photo-1564308963573-dedc7e72e13f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80
+//          https://images.unsplash.com/photo-1559562351-77ae2247f5a3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1631&q=80
+//          https://images.unsplash.com/photo-1602711153055-ee5c0a48bc63?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1954&q=80
+
+//Skara Brae
+
+//         https://www.northlinkferries.co.uk/wp-content/uploads/2021/11/skara-brae-neolithic-village.jpg
+//         https://upload.wikimedia.org/wikipedia/commons/thumb/b/b0/Skara_Brae_12.jpg/1280px-Skara_Brae_12.jpg
+//         https://upload.wikimedia.org/wikipedia/commons/thumb/d/d2/Skara_Brae_house_1_5.jpg/1280px-Skara_Brae_house_1_5.jpg
+
+//Callanish standing stones
+
+//            https://upload.wikimedia.org/wikipedia/commons/c/c6/Callanish_standing_stones_1.jpg
+//            https://www.historicenvironment.scot/media/2120/cal1-000235_o1.jpg?crop=0,0.18285458008504607,0,0.019838545357902158&cropmode=percentage&width=800&height=480&rnd=132723871890000000
+//            https://images.unsplash.com/photo-1533410189475-f8ea9fa082ee?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80
+
+//Grianan of Aileach
+
+//          https://upload.wikimedia.org/wikipedia/commons/0/06/Griahan_of_aileach.jpg
+//          https://images.unsplash.com/photo-1603522961978-c030b89999c8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1528&q=80
+//          http://www.megalithicireland.com/Donegal/Grianan%20of%20Ailech%20001L.jpg
+
+//Giant’s Ring
+
+//       https://eu-assets.simpleview-europe.com/northernireland/imageresizer/?image=%2Fdmsimgs%2F073EF6FB6FCFE1C5C527391866D0D30337D2604D.jpg&action=ProductDetailImage  
+//       https://upload.wikimedia.org/wikipedia/commons/thumb/e/e8/Northern_Ireland_Belfast_Giants_Ring_001.jpg/1920px-Northern_Ireland_Belfast_Giants_Ring_001.jpg
+//       https://www.irishnews.com/picturesarchive/irishnews/irishnews/2017/06/21/122114344-722f23dc-3721-4c6f-a4f0-1b75d58b5ab7.jpg
+
+
+
+
+// =======================================================================
+// sources
+// ==========================================================================
+
+// nationaltrust
+// englishheritage
+// wikipedia
+// https://www.northlinkferries.co.uk/orkney-blog/skara-brae/
+// unsplash
+// https://www.historicenvironment.scot/
+// http://www.megalithicireland.com/
+// https://discovernorthernireland.com/
+// https://www.irishnews.com/lifestyle/2017/06/24/news/take-on-nature-small-oases-of-wildlife-flourish-within-belfast-s-city-boundaries-1062913/
+
+
+// ========================================================================
