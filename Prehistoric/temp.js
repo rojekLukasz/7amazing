158af0c768bbeb8841741406ad4a8a46224d2a7e
rojekLukasz <lukasz_grzegorz@purplebeard.co.uk>
2022-03-22 15:13:30 +0000

    tempWeatherReady

--- Prehistoric/temp.js
+++ Prehistoric/temp.js
@@ -43,3 +43,26 @@ leftIcon1.addEventListener('click', function() {
 //     }
 //   })
 // }
+
+// Api key: 4e5df4075d9195ca31e1b3452e71b178
+
+const keyAPI = '4e5df4075d9195ca31e1b3452e71b178';
+
+let loc = 'Marlborough,GB';
+
+let passInString = `https://api.openweathermap.org/data/2.5/weather?q=${loc}&appid=${keyAPI}&units=metric`;
+console.log(passInString);
+
+
+
+fetch(passInString)
+  .then((res) => res.json())
+  .then((data) => (responseData = data))
+  .then(() => {
+    console.log(responseData);
+    document.querySelector('.weatherDiscription').innerHTML = 'The current weather: ' + responseData.weather[0].description + '. ';
+    document.querySelector('.iconWeather').innerHTML = `<img src="https://openweathermap.org/img/wn/${responseData.weather[0].icon}.png" alt="The weather icon" width="70" height="60">`
+    document.querySelector('.temp').innerHTML = 'Temp: ' + responseData.main.temp.toFixed(0) + '°C';
+    let mphWind = responseData.wind.speed * 2.23693629;
+    document.querySelector('.wind').innerHTML = 'Wind: ' + mphWind.toFixed(1) + ' mph'
+  })
